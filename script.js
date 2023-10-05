df175d1d54f2469377982f32c4fe3a41ac200fc3
anderutan <52438782+anderutan@users.noreply.github.com>
2023-10-05 20:01:14 +0800

    Update button datatype for easy to read JS
    - Remove the button class of 'operator' and 'number' then change it to
    - datatype so that JS code easy to read for number and operator click
    - event.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const keys = document.querySelector('.calculator_keys');
-const display = document.querySelector('.calculator_display');
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator_keys');
+const display = calculator.querySelector('.calculator_display');
 
 keys.addEventListener('click', (event) => {
   if (!event.target.closest('button')) return;
@@ -8,9 +9,19 @@ keys.addEventListener('click', (event) => {
   const keyValue = key.textContent;
   const displayValue = display.textContent;
 
-  if (displayValue === '0') {
-    display.textContent = keyValue;
-  } else {
-    display.textContent = displayValue + keyValue;
+  // Is this a number keys?
+  if (key.dataset.type === 'number') {
+    if (displayValue === '0') {
+      display.textContent = keyValue;
+    } else {
+      display.textContent = displayValue + keyValue;
+    }
+  }
+
+  // Is this a operator key?
+  if (key.dataset.type === 'operator') {
+    console.log(key);
+
+    calculator.dataset.previousKeyType = 'operator';
   }
 });
